3e3d3599a0d2c507e5320d0d29636051c05efd18
Wafiq Harris-Ashby <848241+whashby@users.noreply.github.com>
2026-04-03 01:10:37 -0400

    Update class-cb-rest-dashboard.php

--- includes/modules/class-cb-rest-dashboard.php
+++ includes/modules/class-cb-rest-dashboard.php
@@ -321,9 +321,8 @@ final class CB_REST_Dashboard {
 
 	public static function sync_health(): array {
 		try {
-			$api = new CB_API();
-			$result = $api->sync();
 
+			$result = CB_API::instance()->sync(absint($r->get_param('count') ?: 100), CB_Constants::OPT_MIN_START_DATE, true);
 			update_option(CB_Constants::OPT_LAST_SYNC, gmdate('Y-m-d H:i:s'));
 
 			return [
